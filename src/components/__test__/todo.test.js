d0ea8087252b7703115ce876d60992ff45a092f7
SmirnovDmitry1 <smirnovdmitry322@gmail.com>
2021-10-29 09:40:48 +0300

    commit

---
+++ src/components/__test__/todo.test.js
@@ -0,0 +1,24 @@
+import { screen, cleanup, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Todo from '../todo'
+
+afterEach(() => {
+    cleanup()
+})
+
+test('should render non-completed todo', () => {
+    const todo = {id: 1, title: 'olololo', completed: false}
+    render(<Todo todo={todo} />)
+    const todoElement = screen.getByTestId('todo-1');
+    expect(todoElement).toBeInTheDocument(); 
+    expect(todoElement).not.toContainHTML('<strike>')
+})
+
+test('should render completed todo', () => {
+    const todo = {id: 2, title: 'alalala', completed: true}
+    render(<Todo todo={todo} />)
+    const todoElement = screen.getByTestId('todo-2');
+    expect(todoElement).toBeInTheDocument(); 
+    expect(todoElement).toContainHTML('</strike>')
+
+})
